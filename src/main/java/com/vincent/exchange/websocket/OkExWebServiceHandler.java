f562f67333947d1da2f5496f3c930702adad4e48
Vincent.Y.K.Jiang <vincent4502237@gmail.com>
2021-08-19 17:36:23 +0800

    FEAT: Send the received message to redis stream channel (#1)

--- src/main/java/com/vincent/exchange/websocket/OkExWebServiceHandler.java
+++ src/main/java/com/vincent/exchange/websocket/OkExWebServiceHandler.java
@@ -5,7 +5,10 @@ import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Response;
 import okhttp3.WebSocket;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.connection.stream.StreamRecords;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -16,6 +19,9 @@ public class OkExWebServiceHandler {
 
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     /**
      * send request to subscribe the tickers channel
@@ -32,15 +38,21 @@ public class OkExWebServiceHandler {
         webSocket.send("{\"op\":\"subscribe\",\"args\":[{\"channel\":\"trades\",\"instId\":\"ETH-USDT\"}]}");
     }
 
+    /**
+     * send the received message to redis stream channel
+     *
+     * @param webSocket
+     * @param text
+     */
     public void doReceiveMessage(WebSocket webSocket, String text) {
         JSON messageJson = JSONUtil.parse(text);
         Object messageData = JSONUtil.getByPath(messageJson, "$.data[0]");
         String channel = JSONUtil.getByPath(messageJson, "$.arg.channel", "").toUpperCase();
         String instrumentId = JSONUtil.getByPath(messageJson, "$.arg.instId", "").toUpperCase();
-        Double timestamp = JSONUtil.getByPath(messageJson, "$.data[0].ts", Double.MIN_VALUE);
         if (messageData != null) {
-            String key = String.format("OKEX-%s-%s", channel, instrumentId);
-            redisTemplate.opsForZSet().add(key, messageData, timestamp);
+            String key = String.format("OKEX-SUBSCRIBE-%s-%s", channel, instrumentId);
+            ObjectRecord<String, Object> objectRecord = StreamRecords.newRecord().ofObject(messageData).withStreamKey(key);
+            stringRedisTemplate.opsForStream().add(objectRecord);
         }
     }
 
